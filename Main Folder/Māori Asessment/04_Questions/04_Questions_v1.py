f59c047b33c5910ea33bad39d10b67045f5a4c8d
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-05-18 14:09:05 +1200

    Update v1.3.12

--- Main Folder/Māori Asessment/04_Questions/04_Questions_v1.py
+++ Main Folder/Māori Asessment/04_Questions/04_Questions_v1.py
@@ -2,5 +2,5 @@ question_1 = input("What is tahi? ")
 
 if question_1 == "one" or "1" or "One":
     print("You got it right!")
-elif question_1 != "One" or "1" or "one":
+else:
     print("You got it wrong. Try again: ")
