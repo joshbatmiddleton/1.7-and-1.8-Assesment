1bdaff36a35018ad10ede7026ffa2ea12c4403d1
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-05-23 12:42:28 +1200

    Update v1.3.6

---
+++ Main Folder/Māori Asessment/04_Questions/04_Questions_v1.py
@@ -0,0 +1,24 @@
+import random
+
+print("Get ready to answer 10 questions!!!")
+
+# 1st list
+numbers_in_words = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+
+# 2nd list
+maori_numbers = ["Tahi", "Rua", "Toru", "Wha", "Rima", "Ono", "Whetu", "Waru", "Iwa", "Tekau"]
+
+question = random. choice(numbers_in_words)
+attempt = input(f"What is the Māori number for {question}: ")
+# Using the index position of the question in one list to find the corresponding
+# index position of the answer
+
+answer_index = numbers_in_words.index(question)
+answer = [answer_index]
+
+# Compare the attempt to see if it matches the correct answer
+if attempt == answer:
+    print("Correct! Well done!")
+
+else:
+    print("Incorrect!\n")
